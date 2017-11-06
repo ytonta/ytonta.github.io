4a177dc55fb22fea24335ace4fc6cb13ccf1f676
Yannick Tonta <yannick.tonta@gmail.com>
2017-11-06 09:30:03 +0400

    Add animejs

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import Anime from 'react-anime';
 import Header from '../components/Header';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="wrapper">
         <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Anime delay={(e, i) => i * 1000}
+               scale={[0, .9]}>
+          <div className="blue"/>
+          <div className="green"/>
+          <div className="red"/>
+        </Anime>
+        <p>container</p>
       </div>
     );
   }
