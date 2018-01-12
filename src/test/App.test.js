70a257208febf4b617795e5d5885ff7d6c1ef62a
Yannick Tonta <yannick.tonta@gmail.com>
2018-01-12 13:33:23 +0400

    Fix test script

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../containers/App';
+import Main from '../components/Main';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Main />, div);
 });
