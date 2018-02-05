6fbd8e58aa08aad6c463b8446f22f2c2cd4e1019
Yannick Tonta <yannick.tonta@gmail.com>
2018-02-05 17:00:16 +0400

    Add header markup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,23 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header>
-        Header
+      <header class='header'>
+        {/* Menu */}
+        <nav class='menu'>
+          <ul class='menu__list'>
+            <li class='menu__item'>Menu 1</li>
+            <li class='menu__item'>Menu 2</li>
+            <li class='menu__item'>Menu 3</li>
+            <li class='menu__item'>Menu 4</li>
+          </ul>
+        </nav>
+
+        {/* Header */}
+        <div class='profile'>
+          <div class='profile__email'>Email</div>
+          <div class='profile__picture'>Photo</div>
+          <div class='profile__social'>Social</div>
+        </div>
       </header>
     );
   }
