7d6b04a23f5d652b550c5b32d0fe9db61fcf30b6
Yannick Tonta <yannick.tonta@gmail.com>
2018-02-06 11:50:34 +0400

    Add styling for header and profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,32 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header class='header'>
+      <header className='header'>
         {/* Menu */}
-        <nav class='menu'>
-          <ul class='menu__list'>
-            <li class='menu__item'>Menu 1</li>
-            <li class='menu__item'>Menu 2</li>
-            <li class='menu__item'>Menu 3</li>
-            <li class='menu__item'>Menu 4</li>
+        <nav className='menu'>
+          <ul className='menu__list'>
+            <li className='menu__item'>
+              <a href='#menu-1'>Menu 1</a>
+            </li>
+            <li className='menu__item'>
+              <a href='#menu-2'>Menu 2</a>
+            </li>
+            <li className='menu__item'>
+              <a href='#menu-3'>Menu 3</a>
+            </li>
+            <li className='menu__item'>
+              <a href='#menu-4'>Menu 4</a>
+            </li>
           </ul>
         </nav>
 
         {/* Header */}
-        <div class='profile'>
-          <div class='profile__email'>Email</div>
-          <div class='profile__picture'>Photo</div>
-          <div class='profile__social'>Social</div>
+        <div className='profile'>
+          <div className='profile__email'>Email</div>
+          <div className='profile__picture'>
+            <img alt='profile' src='http://via.placeholder.com/300x300' />
+          </div>
+          <div className='profile__social'>Social</div>
         </div>
       </header>
     );
---
+++ src/components/Section.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Section extends Component {
+  render() {
+    return <div>Section</div>;
+  }
+}
+
+export default Section;
